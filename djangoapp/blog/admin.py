acd7644264eaadc1776274efa36b3704fd3a84b0
Merctxt <giovannihettwer2@gmail.com>
2024-11-25 19:42:35 -0300

    add_summernotes and resize image

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from blog.models import Tag, Category, Page, Post
+from django_summernote.admin import SummernoteModelAdmin
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
@@ -20,7 +22,8 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 @admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
+class PageAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'slug',
     list_display_links = 'title',
     search_fields = 'id', 'title', 'slug',
@@ -30,7 +33,8 @@ class PageAdmin(admin.ModelAdmin):
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
     list_display = 'id', 'title', 'is_published', 'created_at',
     list_display_links = 'title',
     search_fields = 'id', 'title', 'slug', 'excerpt', 'content', 
@@ -41,3 +45,10 @@ class PostAdmin(admin.ModelAdmin):
     readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by',
     prepopulated_fields = {'slug': ('title',)}
     autocomplete_fields = 'tags', 'category',
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.updated_by = request.user
+        else:
+            obj.created_by = request.user
+        obj.save()
